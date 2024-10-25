0f45939325a8a8f12aafc5694cb14156726ecc39
Hunter Metzger <hunter2000metzger@gmail.com>
2024-10-25 15:02:05 -0400

    Havent Tested, may not run

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from kivy.uix.label  import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
+import math
 
 
     
 class StairCalc(ScreenManager):
     pass
-
 
 
 class MainScreen(Screen):
@@ -21,7 +21,25 @@ class StairCalcApp(App):
         app = StairCalc()
         return app
 
+def flight_calculator(rise_of_fight, run_of_flight, stair_max_height, stair_min_height, stair_max_run, stair_min_run):
+    hypotenuse_of_flight = math.sqrt(rise_of_fight ** 2 + run_of_flight ** 2)
 
+
+def triangle_validator(rise, run):
+    hypotenuse = math.sqrt(rise ** 2 + run ** 2)
+    try:
+        A = math.atan(rise / run)
+    except ZeroDivisionError:
+        return "Tread Depth is Zero"
+    try:
+        B = math.atan(run / rise)
+    except ZeroDivisionError:
+        "Rise is Zero"
+    return {'Angles': [math.pi / 2, A, B], "Sides": [rise, run, hypotenuse]}
+        
+
+x =triangle_validator(12.1, 6)
+print(x)
 if __name__ == '__main__':
     app = StairCalcApp()
-    app.run()
+   # app.run()
