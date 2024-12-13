a7b857991d419680f9883a02c802c4df8e46ef9c
Hunter Metzger <hunter2000metzger@gmail.com>
2024-12-13 13:34:47 -0500

    made the GUI graphical

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from calculation import flight_calc
 from kivy.base import Builder
 Builder.load_file("StairCalc.kv")
 from kivy.core.text import LabelBase
+from kivy.uix.widget import Widget
+from time import sleep
+
 
 # LabelBase.register(name="fonts/cambria.ttc", fn_regular="fonts/cambria.ttc", fn_bold="fonts/cambriab.ttf")
 
@@ -18,10 +21,6 @@ class StairCalc(ScreenManager):
 class StairSize(Screen):
     staircase_height = NumericProperty(0)
     staircase_run = NumericProperty(0)
-    stair_max_height = NumericProperty(0)
-    stair_min_height = NumericProperty(0)
-    stair_max_depth = NumericProperty(0)
-    stair_min_depth = NumericProperty(0)
     number_of_stairs = NumericProperty(0)
     hypotenuse = NumericProperty(0)
     iteration = NumericProperty(0)
@@ -36,69 +35,47 @@ class StairSize(Screen):
             return None
 
     def submit(self):
-        # Validate inputs
-        inputs = [
-            ("StaircaseHeight", "staircase_height"),
-            ("StaircaseDepth", "staircase_run"),
-            ("MaxStairHeight", "stair_max_height"),
-            ("MinStairHeight", "stair_min_height"),
-            ("MaxStairDepth", "stair_max_depth"),
-            ("MinStairDepth", "stair_min_depth"),
-        ]
-        for widget_id, attr in inputs:
-            value = self.validate_input(self.ids[widget_id].text)
-            if value is None:
-                self.ids.MainLabel.text = "Invalid input!"
-                self.ids.MainLabel.color = [1, 0, 0, 1]
-                return
-            setattr(self, attr, value)
+        try:
+            self.staircase_run = float(self.ids.StairCaseX.text)
+            self.staircase_height = float(self.ids.StairCaseY.text)
+            self.number_of_stairs = int(self.ids.NumberofStairs.text)
+        except ValueError:
+            self.staircase_run = 100
+            self.staircase_height = 100
+            self.number_of_stairs = 20
+        
 
-        # Calculate solutions
-        self.iteration = 0
-        self.solutions = flight_calculator(
-            self.staircase_height,
-            self.staircase_run,
-            self.stair_max_height,
-            self.stair_min_height,
-            self.stair_max_depth,
-            self.stair_min_depth,
-        )
-        if not self.solutions:
-            self.ids.MainLabel.text = "No solutions found!"
-            self.ids.MainLabel.color = [1, 0, 0, 1]
-            return
+        self.build_staircase(self.number_of_stairs)
 
-        # Update the display
-        self.hypotenuse = math.sqrt(self.staircase_height ** 2 + self.staircase_run ** 2)
-        self.update_solution()
+    def build_staircase(self, num_of_stairs):
+        # get displayscreen [where the stair graphics will be built]
+        screen = self.ids.DisplayScreen
+        screen.clear_widgets()
+        # calculate Stair width
+        step_width = screen.width / num_of_stairs
+        step_height = screen.height / num_of_stairs
+        display_position = screen.pos
+        for x in range(num_of_stairs):
+            print(x)
+            stair = Step()
+            stair.size = (step_width, step_height)
+            stair.pos = (display_position[0] + (step_width * x)), (display_position[1] + (step_height * x))
+            screen.add_widget(stair)
 
-    def next(self):
-        """Show the next solution."""
-        if not self.solutions:
-            return
-        self.iteration = (self.iteration + 1) % len(self.solutions)
-        self.update_solution()
-
-    def update_solution(self):
-        """Update the displayed solution."""
-        solution = self.solutions[self.iteration]
-        self.ids.Tread_Depth.text = f"{solution[0]:.2f}"
-        self.ids.Stair_Height.text = f"{solution[1]:.2f}"
-        self.number_of_stairs = round(self.hypotenuse / solution[2], 2)
-        self.ids.NumberofStairs.text = f"{self.number_of_stairs}"
-        self.ids.MainLabel.text = "Stair Calulator[Fixed Staircase Depth]"
-
-    # Additional methods can be added here for future features
 
 class StartScreen(Screen):
     pass
 
+class Step(Widget):
+    pass
+
+
 class Flight_Calculator(Screen):
-    flight_height = None
-    flight_depth = None
-    stair_height = None
-    stair_depth = None
-    number_of_stairs = None
+    flight_height = NumericProperty
+    flight_depth = NumericProperty
+    stair_height = NumericProperty
+    stair_depth = NumericProperty
+    number_of_stairs = NumericProperty()
     def calculate(self):
         try:
             self.flight_height = float(self.ids.Flight_Height.text)
@@ -112,7 +89,31 @@ class Flight_Calculator(Screen):
             self.ids.WarningLabel.color = (1, 0, 0, 1)
         except ZeroDivisionError:
             self.ids.WarningLabel.text = "Cannot Divide by Zero"
+            # self.build_staircase(self.number_of_stairs, self.flight_depth, self.flight_height)
+            self.build_staircase(5, 200, 100)
+
+    def build_staircase(self, num_of_stairs, staircase_x, staircase_y):
+        # get displayscreen [where the stair graphics will be built]
+        screen = self.ids.DisplayScreen
+        # calculate Stair width
+        step_width = screen.width / num_of_stairs
+        step_height = screen.height / num_of_stairs
+        display_position = screen.pos
+        for stair in range(num_of_stairs):
+            print(stair)
+            stair = Step()
+            stair.size = (step_width, step_height)
+            stair.pos = (display_position + (step_width * stair)), (display_position + (step_height * stair))
+
+            
+            
+
         
+class DisplayScreen(Widget):
+    pass
+
+
+
 class StairCalcApp(App):
     def build(self):
         return StairCalc()
@@ -120,3 +121,4 @@ class StairCalcApp(App):
 
 if __name__ == '__main__':
     StairCalcApp().run()
+
